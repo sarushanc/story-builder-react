bad9e674471bc17a1ec4edaca6a46f7aa3e4293e
sharushan <sarushan1994@gmail.com>
2024-07-24 21:47:16 +0530

    first commit

---
+++ src/styles/GlobalStyles.js
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+  body {
+    margin: 0;
+    font-family: Arial, Helvetica, sans-serif;
+  }
+`;
+
+export default GlobalStyles;
